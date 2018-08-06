ff2c3b6dd63c08b4cbf8105cad59a1afcc45704d
yunwuxin <448901948@qq.com>
2018-08-06 14:57:30 +0800

    完善

---
+++ src/auth/traits/PoliciesModel.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yunwuxin
+ * Date: 2018/8/6
+ * Time: 14:44
+ */
+
+namespace yunwuxin\auth\traits;
+
+use yunwuxin\auth\Collection;
+use yunwuxin\facade\Gate;
+
+trait PoliciesModel
+{
+    public function withPolicies($abilities)
+    {
+        if (is_string($abilities)) {
+            $abilities = explode(',', $abilities);
+        }
+
+        $this->withAttr('policies', function () use ($abilities) {
+            $data = [];
+            foreach ($abilities as $ability) {
+                $data[$ability] = Gate::can($ability, $this);
+            }
+            return $data;
+        });
+
+        $this->append(['policies']);
+    }
+
+    public function toCollection($collection, $resultSetType = null)
+    {
+        return parent::toCollection($collection, Collection::class);
+    }
+}
